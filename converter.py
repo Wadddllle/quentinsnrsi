5e2393239e85e363a1e9e51158c57e4174bf8cae
quentin <quencanteen@gmail.com>
2026-09-04 16:50:18 +0800

    cleanup

--- converter.py
+++
@@ -1,9 +0,0 @@
-import requests
-    
-url = "https://www.onemap.gov.sg/api/common/convert/4326to3857?latitude=1.29441667 &longitude=103.77933333"
-    
-headers = {"Authorization": "**********************"}
-    
-response = requests.request("GET", url, headers=headers)
-    
-print(response.text)
